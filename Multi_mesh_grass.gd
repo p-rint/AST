fca34036072bbd871143c0895ff91c6bf2d8e36c
p-rint <159627419+p-rint@users.noreply.github.com>
2026-02-03 19:25:17 -0500

    Fixed grass, it;'s a bit better now!!!
    Made grass moving work, added color, animation so nice!!!!!

--- Multi_mesh_grass.gd
+++ Multi_mesh_grass.gd
@@ -19,11 +19,13 @@ func multi():
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D
 		
 	multimesh.instance_count = 1000
-	multimesh.visible_instance_count = 100
+	multimesh.visible_instance_count = 1000
 		
 	var index = 0
-	for i in 10:
-		for j in 10:
-			multimesh.set_instance_transform(index, Transform3D(Basis(), Vector3(i * .5,0, j * .5)))
+	for i in 30:
+		for j in 30:
+			var b = Basis().rotated(Vector3.UP, randf_range(0, PI * 2))
+			var t = Transform3D(b, Vector3(i * .5,0, j * .5))
+			multimesh.set_instance_transform(index, t)
 			print(i)
 			index += 1
